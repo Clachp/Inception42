5532d436692ab6904c0ebea14d3c330b1cea6d88
claire <claire@inception.com>
2023-08-01 17:19:51 +0200

    configuring wordpress

--- srcs/requirements/wordpress/conf/auto.sh
+++ srcs/requirements/wordpress/conf/auto.sh
@@ -5,7 +5,7 @@ DB_NAME=${2-'wordpress'}
 DB_USER=${3-'root'}
 DB_PASS=${4-'password'}
 
-SITE_URL=${5-'https://wordpress.test'}
+SITE_URL=${5-'https://www.elle.fr'}
 SITE_TITLE=${6-'WordPress Site in a Subdirectory'}
 SITE_USER=${7-'admin'}
 SITE_PASS=${8-'password'}
@@ -40,23 +40,28 @@ adduser $SITE_USER
 su $SITE_USER
 
 # download WordPress files
-wp core download
+# wp core download
 
 # create the wp-config.php file
-wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS
-1/10 [--dbpass=<dbpass>]: type_your_password
+wp config create --dbname=$DB_NAME \
+    --dbuser=$DB_USER \
+    --dbpass=$DB_PASS \
+    --dbhost=mariadb:3306 \
+    --path='/wordpress'
+1/10 [--dbpass=<dbpass>]:
 Success: Generated 'wp-config.php' file.
+
 
 # create the database
 wp db create
-Success: Database created.
+Success: echo 'Database created.'
 
 # install WordPress (less than 5 mins)
 wp core install --url=$SITE_URL --title="$SITE_TITLE" --admin_user=$SITE_USER --admin_password=$SITE_PASS --admin_email=$SITE_EMAIL
 
 # add second user TODO
 # wp user create
-
+echo 'core install done'
 # Copy (not move) index.php file to root
 cd ../
 ls 
