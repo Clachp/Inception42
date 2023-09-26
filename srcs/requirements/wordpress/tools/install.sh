959c32da2b9706ff71a44022f587469d4bb6ef61
claire <claire@cchapon.debian.com>
2023-09-26 19:00:37 +0200

    config wordpress

---
+++ srcs/requirements/wordpress/tools/install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ -f ./wordpress/wp-config.php ]
+then
+    echo "wordpress already exists"
+else
+
+wp core download --allow-root
+
+# create the wp-config.php file
+wp config create --dbname=$MYSQL_DATABASE \
+    --dbuser=$MYSQL_USER \
+    --dbpass=$MYSQL_PASSWORD \
+    --dbhost=localhost --allow-root
+
+# install WordPress (less than 5 mins)
+wp core install --allow-root --url=cchapon.42.fr \
+	--title=Inception \
+	--admin_user=blabla\
+	--admin_password=blabla \
+	--admin_email=blablabla
+
+echo 'core install done'
+
+echo 'Install finished!'
+
+fi
+
+exec "$@"
