e75462f90b1113a51b53abe6b9d5c50d0fb2bc23
cchapon <claire_1105@hotmail.fr>
2023-11-07 18:55:50 +0100

    pb connection mariadb

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -4,15 +4,15 @@
 service mysql start;
 
 # log into MariaDB as root and create database and the user
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
+#mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+#mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+#mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+#mysql -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+#mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
 #mysqladmin -u root shutdown
-exec mysqld_safe
+#exec mysqld_safe
 
 #print status
 echo "MariaDB database and user were created successfully! "
