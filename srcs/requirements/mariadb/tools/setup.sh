3d2d94fd43ee7acbb091e77ea54b2ea2e7019c5a
cchapon <claire_1105@hotmail.fr>
2023-11-08 19:04:44 +0100

    progress

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-#set -eux
 
 service mysql start;
 
 # log into MariaDB as root and create database and the user
-#mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-#mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-#mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-#mysql -e "FLUSH PRIVILEGES;"
-
-#mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
-#mysqladmin -u root shutdown
-#exec mysqld_safe
+mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "SELECT user,host FROM mysql.user;"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY \`${MYSQL_ROOT_PASSWORD}\`';"
+mysql -e "FLUSH PRIVILEGES;"
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+#mysql -e "SHOW DATABASES;"
+mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+exec mysql_safe
+#kill $(cat /var/run/mysqld/mysqld.pid)
+#mysqld
+mysql -e "SHOW DATABASES;"
 
 #print status
-echo "MariaDB database and user were created successfully! "
+echo "MariaDB database and user were created successfully!"
