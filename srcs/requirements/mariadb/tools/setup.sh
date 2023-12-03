757b859f6cae1dd603545ed908e0c49b7c0fe9c9
cchapon <claire_1105@hotmail.fr>
2023-12-03 16:15:54 +0100

    modif mariadb setup

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -3,13 +3,13 @@
 
 service mariadb start;
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
-
-mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-
-mariadb -e "GRANT ALL PRIVILEGES ON *.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-
 mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY \`${DB_ROOT_PASSWORD}\`';"
+
+mariadb -u root -p${DB_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+
+mariadb -u root -p${DB_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+
+mariadb -u root -p${DB_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON *.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
 
 mariadb -u root -p${DB_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 
