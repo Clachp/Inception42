f0963594578e4d5ca0e21b2cf71584ffaa24d0a1
Claire CHAPON <cchapon@e3r3p1.clusters.42paris.fr>
2023-12-03 12:35:19 +0100

    to test

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-service mysql start;
+service mariadb start;
 
 if [ -d "/var/lib/mysql/${DB_NAME}" ]
 then
 
 	echo "Database already exists"
-#	mysql -uroot -p${DB_ROOT_PASSWORD} -e "SHOW DATABASES;"
+#	mariadb -uroot -p${DB_ROOT_PASSWORD} -e "SHOW DATABASES;"
 
 else
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
 
-mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
 
-mysql -e "SELECT user,host FROM mysql.user;"
+mariadb -e "SELECT user,host FROM mariadb.user;"
 
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON *.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY \`${DB_ROOT_PASSWORD}\`';"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY \`${DB_ROOT_PASSWORD}\`';"
 
-mysql -u root -p${DB_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+mariadb -u root -p${DB_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 
 echo "MariaDB database and user were created successfully!"
 
--- srcs/requirements/wordpress/tools/install.sh
+++ srcs/requirements/wordpress/tools/install.sh
@@ -9,31 +9,26 @@ else
 	mkdir -p wordpress
 	cd wordpress
 
-	wp core download --allow-root
+	##wp core download --allow-root
 
 	wp config create --allow-root \
 		--dbname=$DB_NAME \
 		--dbuser=$DB_USER \
 		--dbpass=$DB_PASSWORD \
-		--dbhost=$DB_HOST \
+		--dbhost=$DB_HOST
+		--url=https://$DOMAIN_NAME;
 
 	wp core install	--allow-root \
-		--url=$DOMAIN_NAME \
+		--url=https://$DOMAIN_NAME \
 		--title=$SITE_TITLE \
 		--admin_user=$ADMIN_USER \
 		--admin_password=$ADMIN_PASSWORD \
 		--admin_email=$ADMIN_EMAIL;
 
-	wp user create --allow-root $USER_LOGIN $USER_MAIL\
+	wp user create --allow-root $USER_LOGIN $USER_MAIL \
 		--role=editor \
 		--user_pass=$USER_PASSWORD;
 
 	wp cache flush --allow-root
-
-# it provides an easy-to-use interface for creating custom contact forms and managing submissions, as well as supporting various anti-spam techniques
-#wp plugin install contact-form-7 --activate
-
-# set the site language to English
-#wp language core install en_US --activate
 
 fi
