8a988b06e5413bfebf9888637b6064d230c0686a
cchapon <claire_1105@hotmail.fr>
2023-12-04 12:35:44 +0100

    working

--- srcs/requirements/wordpress/tools/install.sh
+++ srcs/requirements/wordpress/tools/install.sh
@@ -1,34 +1,49 @@
 #!/bin/bash
 #set -eux
 
-if [ ! -d /run/php ]; then
+sleep 10
+
+if [ ! -d "/run/php" ]; then
 	mkdir /run/php;
 fi
 
-#cd /var/www/html/wordpress
+echo "Checking for wordpress installation..."
+# Download wordpress if isn't already done
+if [ ! -f "wp-config-sample.php" ]; then
+	echo "=> Downloading wordpress..."
+	wp core download --allow-root;
+	echo "=> Done!"
+fi
 
-if ! wp core is-installed --allow-root; then
+echo "Checking for wordpress configuration..."
 
+if [ ! -f "wp-config.php" ]; then
+	
+	echo "=> Create config file..."
 	wp config create --allow-root \
 		--dbname=$DB_NAME \
 		--dbuser=$DB_USER \
 		--dbpass=$DB_PASSWORD \
 		--dbhost=$DB_HOST \
-		--path='/var/www/html/wordpress';
-		#--url=https://$DOMAIN_NAME;
+		--path='/var/www/html/'
 
+	echo "Wordpress config file installed"
+
+	echo "=> Installing wordpress..."
 	wp core install	--allow-root \
-		--url=https://$DOMAIN_NAME \
+		--url=$DOMAIN_NAME \
 		--title=$SITE_TITLE \
 		--admin_user=$ADMIN_USER \
 		--admin_password=$ADMIN_PASSWORD \
 		--admin_email=$ADMIN_EMAIL;
+	echo "Wordpress installed"
 
-	wp user create --allow-root \
-		$USER_LOGIN $USER_MAIL \
-		--user_pass=$USER_PASSWORD 
-
-	wp cache flush --allow-root
+	echo "creating user..."
+	wp user create --allow-root $USER_LOGIN $USER_EMAIL\
+		--user_pass=$USER_PASSWORD;
+	echo "user created"
+	
+	wp cache flush --allow-root;
 
 fi
 
